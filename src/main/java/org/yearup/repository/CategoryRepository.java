164dc5286150e01b2079b1ee0ad661d6b8ace4e9
polinabreha <polinabreha@gmail.com>
2026-06-22 10:15:39 -0400

    add methods to the CategoryRepository

--- src/main/java/org/yearup/repository/CategoryRepository.java
+++ src/main/java/org/yearup/repository/CategoryRepository.java
@@ -4,7 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.yearup.models.Category;
 
+import java.util.List;
+
 @Repository
-public interface CategoryRepository extends JpaRepository<Category, Integer>
-{
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
+    List<Category> findByCategoryId(Long categoryId);
+    void deleteByCategoryId(int categoryId);
 }
