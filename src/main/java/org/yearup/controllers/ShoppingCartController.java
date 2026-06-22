f6686155a3e1c69da6e649ee4c7ee09a017033dd
polinabreha <polinabreha@gmail.com>
2026-06-22 15:54:28 -0400

    updated the ShoppingCartController

--- src/main/java/org/yearup/controllers/ShoppingCartController.java
+++ src/main/java/org/yearup/controllers/ShoppingCartController.java
@@ -1,38 +1,72 @@
 package org.yearup.controllers;
 
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.yearup.models.Product;
 import org.yearup.models.ShoppingCart;
+import org.yearup.models.ShoppingCartItem;
 import org.yearup.models.User;
+import org.yearup.service.ProductService;
 import org.yearup.service.ShoppingCartService;
 import org.yearup.service.UserService;
 
 import java.security.Principal;
 
-// convert this class to a REST controller
-// only logged in users should have access to these actions
+@RestController
+@PreAuthorize("isAuthenticated()")
 public class ShoppingCartController
 {
-    // a shopping cart controller depends on the service layer
-    private ShoppingCartService shoppingCartService;
-    private UserService userService;
+    private final ShoppingCartService shoppingCartService;
+    private final ProductService productService;
+    private final UserService userService;
 
-
+    public ShoppingCartController(ShoppingCartService shoppingCartService, UserService userService, ProductService productService) {
+        this.shoppingCartService = shoppingCartService;
+        this.userService = userService;
+        this.productService = productService;
+    }
 
     // each method in this controller requires a Principal object as a parameter
+    @GetMapping
     public ShoppingCart getCart(Principal principal)
     {
-        // get the currently logged in username
         String userName = principal.getName();
-        // find database user by username
         User user = userService.getByUserName(userName);
         int userId = user.getId();
 
-        // use the shoppingCartService to get all items in the cart and return the cart
-        return null;
+        return shoppingCartService.getByUserId(userId);
     }
 
-    // add a POST method to add a product to the cart - the url should be
-    // https://localhost:8080/cart/products/15  (15 is the productId to be added)
-    // return the updated cart with status 201 Created
+    @PostMapping("/cart/products/{productId}")
+    public ResponseEntity<ShoppingCart> addProduct(Principal principal, @PathVariable int productId)
+    {
+        String userName = principal.getName();
+        User user = userService.getByUserName(userName);
+        int userId = user.getId();
+
+        Product product = productService.getById(productId);
+        shoppingCartService.addProduct(userId,product);
+
+
+        ShoppingCart cart = shoppingCartService.getByUserId(userId);
+        return ResponseEntity.status(201).body(cart);
+    }
+
+    @PutMapping("/cart/products/{productId}")
+    public ResponseEntity<ShoppingCart> updateCart(Principal principal, @PathVariable int productId, @RequestBody ShoppingCartItem item)
+    {
+        String userName = principal.getName();
+        User user = userService.getByUserName(userName);
+        int userId = user.getId();
+
+        shoppingCartService.updateProduct(userId, productId, item.getQuantity());
+
+
+        ShoppingCart cart = shoppingCartService.getByUserId(userId);
+        return ResponseEntity.ok().body(cart);
+    }
 
 
     // add a PUT method to update an existing product in the cart - the url should be
