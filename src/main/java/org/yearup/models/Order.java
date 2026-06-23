882590f2e501bd540c42d93e305dca8e3d7c04bb
polinabreha <polinabreha@gmail.com>
2026-06-23 12:26:22 -0400

    created order model for checkout

---
+++ src/main/java/org/yearup/models/Order.java
@@ -0,0 +1,95 @@
+package org.yearup.models;
+
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "orders")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "order_id")
+    private Integer orderId;
+    @Column(name = "data" , nullable = false)
+    private LocalDateTime data;
+    @Column(name = "address", nullable = false)
+    private String address;
+    @Column(name = "city", nullable = false)
+    private String city;
+    @Column(name = "state", nullable = false)
+    private String state;
+    @Column(name = "zip", nullable = false)
+    private String zip;
+    @Column(name = "shipping_amount" ,nullable = false)
+    private BigDecimal shippingAmount;
+
+    public Order(Integer orderId, LocalDateTime data, String address, String city, String state, String zip, BigDecimal shippingAmount) {
+        this.orderId = orderId;
+        this.data = data;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.shippingAmount = shippingAmount;
+    }
+
+    public Integer getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Integer orderId) {
+        this.orderId = orderId;
+    }
+
+    public LocalDateTime getData() {
+        return data;
+    }
+
+    public void setData(LocalDateTime data) {
+        this.data = data;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public BigDecimal getShippingAmount() {
+        return shippingAmount;
+    }
+
+    public void setShippingAmount(BigDecimal shippingAmount) {
+        this.shippingAmount = shippingAmount;
+    }
+}
