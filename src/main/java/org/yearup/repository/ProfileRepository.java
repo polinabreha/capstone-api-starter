aae29d1ebaf06e4147b63adc6648a65ff7e36447
polinabreha <polinabreha@gmail.com>
2026-06-23 09:43:39 -0400

    added the findByUserId method

--- src/main/java/org/yearup/repository/ProfileRepository.java
+++ src/main/java/org/yearup/repository/ProfileRepository.java
@@ -2,9 +2,13 @@ package org.yearup.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.yearup.models.CartItem;
 import org.yearup.models.Profile;
+
+import java.util.List;
 
 @Repository
 public interface ProfileRepository extends JpaRepository<Profile, Integer>
 {
+    Profile findByUserId(int userId);
 }
